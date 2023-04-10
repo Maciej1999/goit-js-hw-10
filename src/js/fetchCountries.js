a9ad02a4ebc4d17e3d31ce5586c927a2c42485f6
Maciej1999 <maciej.sikora.1999@wp.pl>
2023-04-10 22:48:00 +0200

    End of hw-10

--- src/js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
-
-function fetchCountries(name) {
-  return fetch(
-    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
-  );
-}
-export { fetchCountries };
