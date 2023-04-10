cb64da88060d94b729cbabd4439d698e5a6aaebe
Maciej1999 <maciej.sikora.1999@wp.pl>
2023-04-10 23:02:34 +0200

    End of hw 10

--- src/index.js
+++ src/index.js
@@ -1,68 +1,73 @@
-import './css/styles.css';
-import debounce from 'lodash.debounce';
-import { fetchCountries } from './js/fetchCountries.js';
-import Notiflix from 'notiflix';
+let _ = require('lodash');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
-const input = document.querySelector('#search-box');
-const countriesList = document.querySelector('.country-list');
 
-const createCountry = country => {
-  console.log(countriesList[0]);
-  const li = document.createElement('li');
-  li.innerHTML = `<div class="full-info-container"><img src=${
-    country.flags.svg
-  } /><span class="country-name">${
-    country.name.official
-  }</span><ul><li><span class="country-info">Population: ${
-    country.population
-  }</span></li><li><span class="country-info">Capital city: ${
-    country.capital
-  }</span></li><li><span class="country-info">Languages: ${Object.values(
-    country.languages
-  ).join(', ')}</span></li></ul></div>`;
-  countriesList[0].appendChild(li);
-};
+const inputCountry = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-const removeCountry = () => {
-  let element = document.querySelector('ul');
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-};
-
-const createRollMarkup = data => {
-  return data
-    .map(
-      ({ name, flags }) =>
-        `<a href="http://wikipedia.org/wiki/${name.common}" target="_blank"><li class="list-item" data-name="${name.common}"><img src="${flags.svg}" alt="${name.common}"/>${name.common}</li></a>`
-    )
-    .join('');
-};
-
-const handleInput = async event => {
-  let value = event.target.value.trim();
-  if (value !== '') {
-    const response = await fetchCountries(value);
-    if (response.status === 200) {
-      const countries = await response.json();
-      if (countries && countries.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name'
-        );
-      } else if (countries && countries.length === 1) {
-        removeCountry();
-        countries.forEach(country => createCountry(country));
-      } else if (countries && countries.length >= 2 && countries.length <= 10) {
-        countriesList[0].innerHTML = createRollMarkup(countries);
+const fetchCountries = async name => {
+  return await fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
       }
-    } else if (response.status === 404) {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    }
-  }
-  if (value === '') {
-    removeCountry();
-  }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.length);
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length <= 10 && data.length > 1) {
+        data.forEach(country => {
+          // Lists up to 10 countries that match entered value
+          countryListEl(country);
+        });
+      } else {
+        data.forEach(country => {
+          // Creates html element for country with more detailed information
+          countryInfoDetails(country);
+        });
+      }
+    })
+    .catch(() => Notify.failure('Oops, there is no country with that name'));
 };
 
-input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
+inputCountry.addEventListener(
+  'input',
+  _.debounce(() => {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+    if (inputCountry.value == '') {
+      return;
+    } else {
+      fetchCountries(inputCountry.value.trim());
+    }
+  }, DEBOUNCE_DELAY)
+);
+
+const countryListEl = country => {
+  countryList.insertAdjacentHTML(
+    'beforeend',
+    `
+  <li class="country-list__element">
+    <img src="${country.flags.svg}" alt="${country.flags.svg}"/>
+    <p>${country.name.common}</p>
+   </li>`
+  );
+};
+
+const countryInfoDetails = country => {
+  countryInfo.innerHTML = `
+  <img src="${country.flags.svg}" alt="${country.flags.svg}"/>
+  <h2>${country.name.common}</h2>
+  <p><span class="bold">Capital:</span> ${country.capital}</p>
+  <p><span class="bold">Population:</span> ${country.population}</p>
+  <p><span class="bold">Languages:</span> ${Object.values(country.languages)}</>
+  `;
+};
